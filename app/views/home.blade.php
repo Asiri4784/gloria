66dbcc90302fdd2a6f7ded910f403906dc9ec093
Asiri4784 <adrianus.me.mentor@gmail.com>
2015-11-02 23:12:05 +0700

    New Updates
    Nih Laravelnya Guys!

---
+++ app/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Gloria Mandiri Technology</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="{{asset('butstrep/css/bootstrap.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('css/rvengine.css')}}">
+	<script type="text/javascript" src="{{asset('butstrep/js/bootstrap.min.js') }}" ></script>
+</head>
+<body>
+	@include('template.header')
+	<br/>
+	<br/>
+	<br/>
+	<section id="iota">
+		werjwrojiw
+	</section>
+</body>
+</html>
---
+++ app/views/template/header.blade.php
@@ -0,0 +1,21 @@
+<header>
+<div class="row-fluid">	
+<!-- Desktop -->
+	<nav class = "navbar navbar-default navbar-fixed-top" id ="kinzie">
+		<span class = "col-md-3 pull-left" id="firaini">
+		{{HTML::image ('img/n21-2.jpg', '333', array('width'=>'250', 'id'=>'viri'))}}
+		</span>
+		<center>
+		<!-- Desktop -->
+			<span class="col-xs-6" id="firaini">
+						{{HTML::image ('img/Home.png', '333', array('width'=>'250', 'id'=>'inai'))}}
+						{{HTML::image ('img/Film.png', '333', array('width'=>'250', 'id'=>'inai'))}}
+						{{HTML::image ('img/Cinema.png', '333', array('width'=>'250', 'id'=>'inai'))}}
+			</span>
+		</center>
+		<span class="col-md-3" id="firaini">
+				yoo!
+		</span>
+	</nav>
+</div>
+</header>
