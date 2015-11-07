4a5f6ecde97a37a598ecffc51043e8c5be452442
alvin ega variandi <alvin.ega93@gmail.com>
2015-11-07 20:16:21 +0700

    anus tai

--- app/views/recommendedItem.blade.php
+++ app/views/recommendedItem.blade.php
@@ -9,9 +9,12 @@
   <!-- Collect the nav links, forms, and other content for toggling -->
   <div class="collapse navbar-collapse navbar-ex1-collapse">
     <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Link</a></li>
-      <li><a href="#">Link</a></li>
-      <li><a href="#">Link</a></li>
+      <li><a href="#">Computers</a></li>
+      <li><a href="#">Notebooks</a></li>
+	  <li><a href="#">Netbooks</a></li>
+      <li><a href="#">PCs</a></li>
+	  <li><a href="#">Earphones/Headsets</a></li>
+      <li><a href="#">Accessories</a></li>
     </ul>
   </div><!-- /.navbar-collapse -->
 </nav>
