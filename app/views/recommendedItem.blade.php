7fb701077fe4a909f9261b535f729f89b98151f1
alvin ega variandi <alvin.ega93@gmail.com>
2015-11-07 17:08:01 +0700

    no message

---
+++ app/views/recommendedItem.blade.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
+</head>
+<body>
+<nav class="navbar navbar-default" role="navigation" style = "display:block">
+
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Link</a></li>
+      <li><a href="#">Link</a></li>
+      <li><a href="#">Link</a></li>
+    </ul>
+  </div><!-- /.navbar-collapse -->
+</nav>
+</body>
+</html>
