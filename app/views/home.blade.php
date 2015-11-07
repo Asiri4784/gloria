fa07b6c139e3281b67d1b35cdc64c4af36ae5f4f
alvin ega variandi <alvin.ega93@gmail.com>
2015-11-07 17:56:56 +0700

    layout

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -5,14 +5,16 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="{{asset('butstrep/css/bootstrap.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{asset('css/header.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('css/layout.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
 	<script type="text/javascript" src="{{asset('butstrep/js/bootstrap.min.js') }}" ></script>
 </head>
 <body>
+<div class = "page">
 <div class = "header">
 
 </div>
-<div class = "container">
+<div class = "content">
 <div id= "bestSeller">
 @include('bestseller')
 </div>
@@ -26,6 +28,6 @@
 </div>
 <div class = "footer"></div>
 	
-
+</div>
 </body>
 </html>
