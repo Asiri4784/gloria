6dc1b5ca214eb8100f9653b7dc91fc0df2a9ef6b
bambangalibowo <bambangalibowo@gmail.com>
2015-11-07 18:28:34 +0700

    last

--- app/views/bestseller.blade.php
+++ app/views/bestseller.blade.php
@@ -5,7 +5,21 @@
 	</div>
 	
 	<div id ="item">
-	
+		<div id="box" class="col-md-3">
+		a
+		</div>
+		
+		<div id="box" class="col-md-3">
+		a
+		</div>
+		
+		<div id="box" class="col-md-3">
+		a
+		</div>
+		
+		<div id="box" class="col-md-3">
+		a
+		</div>
 	</div>
 </div>
 </html>
